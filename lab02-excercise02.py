4fa95bdf8e9c233f019183da957686fbd56a4dbf
bethlehem-nune <betelhemnune7@gmail.com>
2025-09-05 11:51:42 -0500

    lab02-excercise02.py

---
+++ lab02-excercise02.py
@@ -0,0 +1,18 @@
+'''
+Author: Bethlehem Nune
+KUID: 3025220
+Date: September 3, 2025
+Lab: Lab 1
+Last modified: September 3, 2025
+Purpose: This program prints my name, major, and hobbies on separate lines.
+
+'''
+
+print("my name is Bethlehem nune.")
+print("I am an EECS major.")
+print("My hobbies are:")
+print("\tspending time with my family")
+print("\tWatching movie")
+print("\tReading")
+print("\tCooking")
+print("Goodbye")
